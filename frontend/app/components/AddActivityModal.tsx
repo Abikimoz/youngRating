1c2fc891d0c812fed8740daaf73ff974aafd4e08
Abikimoz <i.m.anisimov25@gmail.com>
2025-10-16 20:59:18 +0300

    add title

--- frontend/app/components/AddActivityModal.tsx
+++ frontend/app/components/AddActivityModal.tsx
@@ -42,7 +42,7 @@ const AddActivityModal: React.FC<AddActivityModalProps> = ({ isOpen, onClose, on
             <label htmlFor="name" className="block text-sm font-medium text-gray-700">Наименование мероприятия</label>
             <select name="name" id="name" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" required>
               {activityNames[selectedCategory]?.map(name => (
-                <option key={name} value={name}>{name}</option>
+                <option key={name} value={name} title={name}>{name}</option>
               ))}
             </select>
           </div>
