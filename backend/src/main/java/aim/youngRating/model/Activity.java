ef5f09268673cf65dd83927b05bd63a26ebcb2b1
Abikimoz <i.m.anisimov25@gmail.com>
2025-10-16 20:29:12 +0300

    fix activities

--- backend/src/main/java/aim/youngRating/model/Activity.java
+++ backend/src/main/java/aim/youngRating/model/Activity.java
@@ -15,6 +15,9 @@ public class Activity {
     @Column(nullable = false)
     private String name;
 
+    @Column
+    private String description;
+
     @Column(nullable = false)
     private LocalDate date;
 
@@ -52,6 +55,14 @@ public class Activity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public LocalDate getDate() {
